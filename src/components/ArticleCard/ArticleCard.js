a158f7ae293935bff607895cbe8311a4d508a05c
John Kiernan <jkiernan12@yahoo.com>
2022-02-22 17:48:45 -0500

    Add style tweaks to card and section

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -5,8 +5,8 @@ function ArticleCard({article}) {
     <article className='article-container'>
       <div className='article-container--text'>
         <h4>{article.title}</h4>
-        <p>{article.byline}</p>
-        <p>{article.abstract}</p>
+        <p className='byline'>{article.byline}</p>
+        <p className='abstract'>{article.abstract}</p>
       </div>
       <img className='article-container--image' src={article.imageSmall && article.imageSmall.url} />
     </article>
