2ded50e894acb9d8373b78b20b17b91c9d6cd94d
John Kiernan <jkiernan12@yahoo.com>
2022-02-23 10:08:07 -0500

    Adjust fetched articles based on url location

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -7,13 +7,21 @@ import { validateCategory, toTitleCase } from '../../utils'
 
 function MainSection({category}) {
   const location = useLocation()
+  console.log(location)
   const [articles, setArticles] = useState([])
   const [currCategory, setCurrCategory] = useState(category)
+  const [maxArticles, setMaxArticles] = useState(5)
 
   useEffect(() => {
+    if (location.pathname.includes('category/')) {
+      setMaxArticles(() => 20)
+    } else {
+      setMaxArticles(() => 5)
+    }
     if (validateCategory(currCategory)) {
       getSection(currCategory)
       .then(data => {
+        console.log('fetch ran')
         const newArticles = [...articles, ...data]
         setArticles(() => newArticles)
       })
@@ -24,7 +32,7 @@ function MainSection({category}) {
     <section className='section-container'>
       <h2>{currCategory && toTitleCase(currCategory)}</h2>
       {articles.length > 0 && articles.map((article, i) => {
-        if (i < 5) {
+        if (i < maxArticles) {
           return <ArticleCard key={article.url} article={article} />
         }
       })}
